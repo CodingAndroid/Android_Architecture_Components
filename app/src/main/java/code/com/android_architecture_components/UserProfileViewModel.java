793d49889b32132e0483cb50433799b3610ae7c5
lihui1 <1316994947@qq.com>
2017-12-29 16:13:01 +0800

    Initial commit

---
+++ app/src/main/java/code/com/android_architecture_components/UserProfileViewModel.java
@@ -0,0 +1,10 @@
+package code.com.android_architecture_components;
+
+import android.arch.lifecycle.ViewModel;
+
+/**
+ * Created by lihui1 on 2017/12/29.
+ */
+
+public class UserProfileViewModel extends ViewModel{
+}
